73abc98403e0d9f63f63c109994e99d00da1189f
Your Name <you@example.com>
2019-07-17 14:42:18 +0530

    add signin,signup pages,

--- app.js
+++ app.js
@@ -3,24 +3,41 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressHbs= require('express-handlebars');
-let db=require('./dbconfig/db-connect')
+let expressHBS= require('express-handlebars');
+let session=require('express-session');
+let passport=require('passport');
+let flash=require('connect-flash');
+
+let db=require('./dbconfig/db-connect');
+
+
 var indexRouter = require('./routes/index');
 
-
+let userRouter=require('./routes/user');
 var app = express();
 
 // view engine setup
-app.engine('.hbs',expressHbs({defaultLayout:'layout',extname:'.hbs'}));
+app.engine('.hbs',expressHBS({defaultLayout: 'layout',extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret:'mysecret',resave:false,saveUninitialized:false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req,res,next) {
+  res.locals.login=req.isAuthenticated();
+  next();
+})
+
 app.use('/', indexRouter);
+app.use('/user',userRouter);
+//app.use('/user/signup',indexRouter);
 
 
 // catch 404 and forward to error handler
@@ -38,16 +55,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-db.connect(function (error) {
-  if(error)
-  {
-    console.log("unable to connect");
-    process.exit(1);
 
-  }
-  else{
-    console.log("Shoping cart Database connect Successfully....");
+db.connect(function (error) {
+
+  if (error){
+    console.log('Unable to connect database');
+    process.exit(1);
+  } else {
+    console.log('Shopping cart Database connecetd successfully...');
   }
 
 });
+
 module.exports = app;
